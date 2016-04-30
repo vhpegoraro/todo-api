577087be6cf20778bafdac6c4108a437f0789dca
Victor Hugo <vh.pegoraro@gmail.com>
2016-04-30 10:52:08 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var PORT = 3000;
+
+app.get("/", function(req, res) {
+   
+   res.send("TODO API Root"); 
+});
+
+app.listen(PORT, function() {
+    
+   console.log("Server is up and running!");
+   console.log("Listening on port "+PORT+" ..."); 
+});
