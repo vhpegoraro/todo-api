5d74e87af272a8a187d2b02e0513375461c730a2
Victor Hugo <vh.pegoraro@gmail.com>
2016-04-30 11:20:50 -0300

    Adding VSCode Node and Express sources

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = createTodos();
 
 app.get("/", function(req, res) {
    
    res.send("TODO API Root"); 
 });
 
-app.listen(PORT, function() {
+app.get("/todos", function(req, res) {
+   
+   res.json(todos); 
+});
+
+app.listen(PORT, function() {     
     
    console.log("Server is up and running!");
    console.log("Listening on port "+PORT+" ..."); 
 });
+
+function createTodos() {
+    
+    var todo1 = {id: 1, description: "Morning lunch", done: true};
+    var todo2 = {id: 2, description: "Play videogame", done: true};
+    var todo3 = {id: 3, description: "Study Node.js", done: false};
+    var todos = [todo1, todo2, todo3];
+    
+    return todos;
+};
