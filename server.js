64e65e5b64c61a8706f619dd05815fc8bf42360b
Victor Hugo <vh.pegoraro@gmail.com>
2016-04-30 12:44:48 -0300

    Added POST method

--- server.js
+++ server.js
@@ -1,21 +1,89 @@
+/**
+ * @author Victor Hugo
+ * @since 30/04/2016
+ * 
+ * Node.js TODO API Sample
+ */
 var express = require("express");
+var bodyParser = require("body-parser");
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = createTodos();
+var todos = getAll();
 
+app.use(bodyParser.json());
+
+/**
+ * Get API root
+ */
 app.get("/", function(req, res) {
    
    res.send("TODO API Root"); 
 });
 
+/**
+ * Get all todos
+ */
 app.get("/todos", function(req, res) {
    
    res.json(todos); 
 });
 
+/**
+ * Get a todo by its id
+ */
 app.get("/todos/:id", function(req, res) {
     
     var id = parseInt(req.params.id, 10);
+    var todo = findById(id);        
+    
+    if (todo) {
+        res.json(todo);
+    } else
+        res.status(404).send();                 
+});
+
+/**
+ * Create a new todo
+ */
+app.post("/todos", function(req, res) {
+   
+    var todo = req.body;
+    
+    if (todo) {
+        save(todo);
+        res.status(201).send();
+    } else
+        res.status(406).send();    
+});
+
+/**
+ * Start server
+ */
+app.listen(PORT, function() {     
+    
+   console.log("Server is up and running!");
+   console.log("Listening on port "+PORT+" ..."); 
+});
+
+/**
+ * Create and return all todos
+ */
+function getAll() {
+    
+    var todo1 = {id: 1, description: "Morning lunch", done: true};
+    var todo2 = {id: 2, description: "Play videogame", done: true};
+    var todo3 = {id: 3, description: "Study Node.js", done: false};
+    var todos = [todo1, todo2, todo3];
+    
+    return todos;
+};
+
+/**
+ * Find and return a todo by id
+ */
+function findById(id) {
+    
     var todo = null;
     
     todos.forEach(function(t) {
@@ -25,24 +93,23 @@ app.get("/todos/:id", function(req, res) {
         }                
     });
     
-    if (todo) {
-        res.json(todo);
-    } else
-        res.status(404).send();                 
-});
+    return todo;
+};
 
-app.listen(PORT, function() {     
+/**
+ * Save a new todo
+ */
+function save(todo) {
     
-   console.log("Server is up and running!");
-   console.log("Listening on port "+PORT+" ..."); 
-});
-
-function createTodos() {
+    var id = todos.length;
     
-    var todo1 = {id: 1, description: "Morning lunch", done: true};
-    var todo2 = {id: 2, description: "Play videogame", done: true};
-    var todo3 = {id: 3, description: "Study Node.js", done: false};
-    var todos = [todo1, todo2, todo3];
+    if (id == 0) {
+        id = 1;
+    } else 
+        id++;
     
-    return todos;
+    todo.id = id;
+          
+    todos.push(todo);
+    
 };
