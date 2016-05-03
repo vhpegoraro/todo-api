3494736220a31c1c74e6ccceb0c7cf31e97f3909
Victor Hugo <vh.pegoraro@gmail.com>
2016-05-03 10:18:20 -0300

    Modularize Routes

---
+++ routes/todos.js
@@ -0,0 +1,92 @@
+var Todo = require(".././models/todo");
+
+module.exports = function(app) {
+    
+    /**
+     * Get all todos
+     */
+    app.get("/todos", function(req, res) {      
+    
+        Todo.find({}, function(err, todos) {
+            
+            if (err) 
+                res.status(500).send();
+            
+            res.json(todos);                
+        });       
+    });
+
+    /**
+     * Get a todo by its id
+     */
+    app.get("/todos/:id", function(req, res) {    
+        
+        Todo.findById(req.params.id, function(error, todo) {
+        
+            if (error)
+                res.status(400).send();
+            
+            if (todo) {
+                res.json(todo);
+            } else
+                res.status(404).send();
+        });                                 
+    });
+
+    /**
+     * Create a new todo
+     */
+    app.post("/todos", function(req, res) {
+    
+        var newTodo = Todo({
+            description: req.body.description,
+            done: req.body.done
+        });
+    
+        newTodo.save(function(error) {
+        
+            if (error) {
+                res.status(400).send();
+                return;
+            }                  
+            
+            res.status(200).send();                    
+        });            
+    });
+
+    /**
+     * Delete a todo
+     */
+    app.delete("/todos/:id", function(req, res) {
+        
+        Todo.findByIdAndRemove(req.params.id, function(error, todo) {
+        
+        if (error)
+            res.status(400).send();
+            
+            if (todo) {
+                res.status(200).send();
+            } else {
+                res.status(404).send();
+            }            
+        });
+    });
+
+    /**
+     * Update a todo
+     */
+    app.put("/todos", function(req, res) {
+        
+        Todo.findByIdAndUpdate(req.body._id, { $set: req.body }, function(error, todo) {       
+        
+        if (error)
+            res.status(400).send();
+            
+            if (todo) {
+                res.status(200).send();
+            } else {
+                res.status(404).send();
+            }            
+        });
+    });
+};
--- server.js
+++ server.js
@@ -11,11 +11,12 @@ var _ = require("underscore");
 var express = require("express");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+var todoRouter = require("./routes/todos");
+
 var app = express();
 
 mongoose.connect(DATABASE_URL);
 var db = mongoose.connection;
-var Todo = require("./models/todo");
 
 db.on("error", console.error.bind(console, "MongoDB connection failed ..."));
 
@@ -35,92 +36,9 @@ app.get("/", function(req, res) {
 });
 
 /**
- * Get all todos
+ * Add TODO Routes
  */
-app.get("/todos", function(req, res) {      
-   
-   Todo.find({}, function(err, todos) {
-        
-        if (err) 
-            res.status(500).send();
-        
-        res.json(todos);                
-    });       
-});
-
-/**
- * Get a todo by its id
- */
-app.get("/todos/:id", function(req, res) {    
-    
-    Todo.findById(req.params.id, function(error, todo) {
-       
-       if (error)
-           res.status(400).send();
-        
-        if (todo) {
-            res.json(todo);
-        } else
-            res.status(404).send();
-    });                                 
-});
-
-/**
- * Create a new todo
- */
-app.post("/todos", function(req, res) {
-   
-    var newTodo = Todo({
-        description: req.body.description,
-        done: req.body.done
-    });
-   
-   newTodo.save(function(error) {
-       
-       if (error) {
-           res.status(400).send();
-           return;
-       }                  
-        
-        res.status(200).send();                    
-   });            
-});
-
-/**
- * Delete a todo
- */
-app.delete("/todos/:id", function(req, res) {
-      
-    Todo.findByIdAndRemove(req.params.id, function(error, todo) {
-       
-       if (error)
-           res.status(400).send();
-        
-        if (todo) {
-            res.status(200).send();
-        } else {
-            res.status(404).send();
-        }            
-    });
-});
-
-/**
- * Update a todo
- */
-app.put("/todos", function(req, res) {
-    
-    Todo.findByIdAndUpdate(req.body._id, { $set: req.body }, function(error, todo) {       
-       
-       if (error)
-           res.status(400).send();
-        
-        if (todo) {
-            res.status(200).send();
-        } else {
-            res.status(404).send();
-        }            
-    });
-});
+todoRouter(app);
 
 /**
  * Start server
